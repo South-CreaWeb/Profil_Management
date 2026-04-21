87ac7b5c3e92fc6c055c13b9ec16a809900fe7bf
South-CreaWeb <south-creaweb@outlook.fr>
2026-04-21 20:01:52 +0200

    Add Backend env

---
+++ backend/src/server.ts
@@ -0,0 +1,31 @@
+import express from 'express'
+import { Profil } from './types/profil.type'
+import { json } from 'node:stream/consumers'
+
+const app = express()
+app.use(express.json())
+const port: number = 3000
+
+const profils: Profil[] = []
+
+app.get('/profils', (request, response) => {
+  response.status(200).json(profils)
+})
+
+app.post('/profils', (request, response) => {
+  const data = request.body
+
+  const newProfil = {
+    id: crypto.randomUUID(),
+    name: data.name,
+    role: data.role,
+  }
+
+  const profil = profils.push(newProfil)
+
+  response.status(201).send(profil)
+})
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`)
+})
